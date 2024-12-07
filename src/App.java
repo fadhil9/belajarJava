e876c61a57b05b709bf12fcd8a595b1348cded56
fadhil9 <fadhilmuhamad272@gmail.com>
2024-12-07 19:50:24 +0700

    while loop

--- src/App.java
+++ src/App.java
@@ -1,7 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
         // murid murid1 = new murid();
-        int nilai = 80;
-        switchTest switch1 = new switchTest(nilai);
+        int nilai = 10;
+        // switchTest switch1 = new switchTest(nilai);
+        // whileLoop whileLoop1 = new whileLoop(nilai);
+
     }
 }
---
+++ src/whileLoop.java
@@ -0,0 +1,9 @@
+public class whileLoop {
+    whileLoop(int nilai){
+        while (nilai>1) {
+            System.out.println("cetak ke : "+nilai);
+            nilai--;            
+        }
+    }
+    
+}
