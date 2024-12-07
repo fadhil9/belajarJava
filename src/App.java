215d0c81f40aa6a9be56a9c29042f8dec2d7a24b
fadhil9 <fadhilmuhamad272@gmail.com>
2024-12-07 19:44:43 +0700

    switch

--- src/App.java
+++ src/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         // murid murid1 = new murid();
-        int nilai =80;
-        switchTest switch1 = new switchTest(nilai)
+        int nilai = 80;
+        switchTest switch1 = new switchTest(nilai);
     }
 }
--- src/switchTest.java
+++ src/switchTest.java
@@ -1,18 +1,20 @@
 public class switchTest {
-    switchTest(nilai){
+    switchTest(int nilai){
         switch(nilai){
             case 90:
-            System.out.println("A");
-            break;
+                System.out.println("A");
+                break;
             case 70:
-            System.out.println("B");
-            break;
+                System.out.println("B");
+                break;
             case 60:
-            System.out.println("C");
-            break;
-            case 70:
-            System.out.println("B");
-            break;
+                System.out.println("C");
+                break;
+            case 50:
+                System.out.println("D");
+                break;
+            default:
+                System.out.println("Nilai Belum Daftar");
         }
     }
 }
