ba83fcdbd671112a883272dfbbf32df48e58ceb2
fadhil9 <fadhilmuhamad272@gmail.com>
2024-12-08 17:45:36 +0700

    method

--- src/App.java
+++ src/App.java
@@ -1,4 +1,13 @@
 public class App {
+    static int perkalian(int a,int b){
+        return a*b;
+        
+    }
+
+    static double pertambahan(double a, double b){
+        return a+b;
+    }
+
     public static void main(String[] args) throws Exception {
         // murid murid1 = new murid();
         // int nilai = 10;
@@ -6,9 +15,20 @@ public class App {
         // whileLoop whileLoop1 = new whileLoop(nilai);
         // Loop loop1 = new Loop(nilai);
         // Loop.forLoop(nilai);
-        String[] mobil = {"mini cooper","ferrari","lamborghini"};
+        // String[] mobil = {"mini cooper","ferrari","lamborghini"};
 
-        Loop.carsLoop(mobil);
+        // Loop.carsLoop(mobil);
+        // myMethod();
+        // myMethod();
+        // myMethod("cungur",170);
+        // myMethod("ijal",150);
+        // myMethod("jaja",165);
+        int hasilPerkalian = perkalian(10, 2);
+        double hasilPertambahan = pertambahan(5.5, 5.9);
+
+        
+        System.out.println(hasilPerkalian);
+        System.out.println(hasilPertambahan);
 
 
     }
