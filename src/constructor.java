d054fe06a7a82cbc60e52d1aa632745f57a402c7
fadhil9 <fadhilmuhamad272@gmail.com>
2024-12-08 20:26:54 +0700

    constructor

---
+++ src/constructor.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class constructor { //class
+    int xx;   //atribut
+
+    public constructor(int y){ //constructor
+         xx = y;
+    }
+    
+    public static void main(String[] args) {
+        Scanner masuk = new Scanner(System.in);
+        System.out.println("masukan inpunt :");
+        int varInput = masuk.nextInt();
+
+
+        constructor kesatu = new constructor(varInput);
+
+        System.out.println("nilainya adalah :"+kesatu.xx);
+    }
+}
